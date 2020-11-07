2b252765b0caaea994d2935faca900a87eabc409
shahin1290 <s.patowary@yahoo.com>
2020-11-07 19:33:54 +0100

    Finishes 2.5: seperate module

--- part-2/courseinfo/src/App.js
+++ part-2/courseinfo/src/App.js
@@ -1,41 +1,5 @@
-import React from 'react'
+import Course from './components/Course'
 
-const Header = ({ name }) => {
-  return <h1>{name}</h1>
-}
-
-const Total = ({ parts }) => {
-  const sum = parts.reduce((acc, part) => acc + part.exercises, 0)
-  return <p>Number of exercises {sum}</p>
-}
-
-const Part = ({ part }) => {
-  return (
-    <p>
-      {part.name} {part.exercises}
-    </p>
-  )
-}
-
-const Content = ({ parts }) => {
-  return (
-    <div>
-      {parts.map((part) => (
-        <Part key={part.id} part={part} />
-      ))}
-    </div>
-  )
-}
-
-const Course = ({ course }) => {
-  return (
-    <div>
-      <Header name={course.name} />
-      <Content parts={course.parts} />
-      <Total parts={course.parts} />
-    </div>
-  )
-}
 const Courses = ({ courses }) => {
   return (
     <div>
---
+++ part-2/courseinfo/src/components/Course.js
@@ -0,0 +1,40 @@
+import React from 'react'
+
+const Header = ({ name }) => {
+  return <h1>{name}</h1>
+}
+
+const Total = ({ parts }) => {
+  const sum = parts.reduce((acc, part) => acc + part.exercises, 0)
+  return <p>Number of exercises {sum}</p>
+}
+
+const Part = ({ part }) => {
+  return (
+    <p>
+      {part.name} {part.exercises}
+    </p>
+  )
+}
+
+const Content = ({ parts }) => {
+  return (
+    <div>
+      {parts.map((part) => (
+        <Part key={part.id} part={part} />
+      ))}
+    </div>
+  )
+}
+
+const Course = ({ course }) => {
+  return (
+    <div>
+      <Header name={course.name} />
+      <Content parts={course.parts} />
+      <Total parts={course.parts} />
+    </div>
+  )
+}
+
+export default Course
