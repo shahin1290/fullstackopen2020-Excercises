897b59cbf55f9416a53a58346bd4f717593d650a
shahin1290 <s.patowary@yahoo.com>
2020-11-08 21:20:29 +0100

    Finishes 2.9: The Phonebook Step4

--- part-2/phonebook/src/App.js
+++ part-2/phonebook/src/App.js
@@ -3,9 +3,13 @@ import React, { useState } from 'react'
 const App = () => {
   const [persons, setPersons] = useState([
     { name: 'Arto Hellas', number: '040-123456' },
+    { name: 'Ada Lovelace', number: '39-44-5323523' },
+    { name: 'Dan Abramov', number: '12-43-234345' },
+    { name: 'Mary Poppendieck', number: '39-23-6423122' },
   ])
   const [newName, setNewName] = useState('')
   const [newNumber, setNewNumber] = useState('')
+  const [searchText, setSearchText] = useState('')
 
   const addPerson = (e) => {
     e.preventDefault()
@@ -30,9 +34,27 @@ const App = () => {
     setNewNumber(e.target.value)
   }
 
+  const Numbers = () => {
+    const filteredPersons = persons.filter((person) =>
+      person.name.toLowerCase().includes(searchText)
+    )
+    return filteredPersons.map((person) => (
+      <div key={person.name}>
+        {person.name} {person.number}
+      </div>
+    ))
+  }
+
+  const handleSearchChange = (e) => {
+    setSearchText(e.target.value)
+  }
+
   return (
     <div>
-      <h2>Phonebook</h2>
+      <h1>Phonebook</h1>
+      filter shown with:
+      <input value={searchText} onChange={handleSearchChange} />
+      <h2>add a New</h2>
       <form onSubmit={addPerson}>
         <div>
           name: <input value={newName} onChange={handleNameChange} />
@@ -44,13 +66,8 @@ const App = () => {
           <button type='submit'>add</button>
         </div>
       </form>
-
       <h2>Numbers</h2>
-      {persons.map((person) => (
-        <div key={person.name}>
-          {person.name} {person.number}
-        </div>
-      ))}
+      <Numbers />
     </div>
   )
 }
