d972e4bff9dd6148a0633c354438391d425fc12b
shahin1290 <s.patowary@yahoo.com>
2020-11-10 21:31:51 +0100

    Finishes 2.18: Phonebook step10

--- part-2/phonebook/src/components/PersonForm.js
+++ part-2/phonebook/src/components/PersonForm.js
@@ -18,24 +18,43 @@ const PersonForm = ({
 
   const handleFormSubmit = (e) => {
     e.preventDefault()
+
     const isNameExists = persons.some(
       (person) => person.name === newName.trim()
     )
 
     if (isNameExists) {
-      return alert(`${newName.trim()} is already added to phonebook`)
+      const personFound = persons.find((person) => person.name === newName)
+      const changedPerson = { ...personFound, number: newNumber }
+      if (
+        window.confirm(
+          `${newName.trim()} is already added to phonebook, replace the old number with a new one?`
+        )
+      ) {
+        personService
+          .update(personFound.id, changedPerson)
+          .then((returnedPerson) => {
+            setPersons(
+              persons.map((person) =>
+                person.id !== personFound.id ? person : returnedPerson
+              )
+            )
+            setNewName('')
+            setNewNumber('')
+          })
+      }
+    } else {
+      personService
+        .create({
+          name: newName.trim(),
+          number: newNumber,
+        })
+        .then((response) => {
+          setPersons(persons.concat(response))
+          setNewName('')
+          setNewNumber('')
+        })
     }
-
-    personService
-      .create({
-        name: newName.trim(),
-        number: newNumber,
-      })
-      .then((response) => {
-        setPersons(persons.concat(response))
-        setNewName('')
-        setNewNumber('')
-      })
   }
 
   return (
--- part-2/phonebook/src/services/persons.js
+++ part-2/phonebook/src/services/persons.js
@@ -11,10 +11,15 @@ const create = (newObject) => {
   return request.then(response => response.data)
 }
 
+const update = (id, newObject) => {
+  const request =  axios.put(`${baseUrl}/${id}`, newObject)
+  return request.then(response => response.data)
+}
+
 const destroy = (id) => {
   const request =  axios.delete(`${baseUrl}/${id}`)
   return request.then(response => response.data)
 }
 
-export default { getAll, create, destroy }
+export default { getAll, create, destroy, update }
  
