e75759c9ac00cb20f4afb52865d79e052945989f
shahin1290 <s.patowary@yahoo.com>
2020-11-16 09:11:16 +0100

    finishes 4.5: helper functions and unit tests, step3

--- part-4/utils/list_helper.js
+++ part-4/utils/list_helper.js
@@ -1,8 +1,19 @@
+const _ = require('lodash')
+
 const dummy = (blogs) => 1
 
 const totalLikes = (blogs) => blogs.reduce((acu, item) => acu + item.likes, 0)
 
+const favoriteBlog = (blogs) => {
+  const blogFound = blogs.reduce((prev, curr) => {
+    return (prev.likes > curr.likes) ? prev : curr
+  })
+  //console.log(_.pick(blogFound, ['author', 'title', 'likes']));
+  return _.pick(blogFound, ['author', 'title', 'likes'])
+}
+
 module.exports = {
   dummy,
   totalLikes,
+  favoriteBlog  
 }
