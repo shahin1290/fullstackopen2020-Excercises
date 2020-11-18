8f7d649781cfaf1c4d5a08c0f5202cd802278448
shahin1290 <s.patowary@yahoo.com>
2020-11-18 18:11:33 +0100

    finishes 4.16: bloglist expansion, step4

--- part-4/controllers/users.js
+++ part-4/controllers/users.js
@@ -1,14 +1,19 @@
 const usersRouter = require('express').Router()
 const User = require('../models/user')
-const bcrypt = require ('bcrypt')
+const bcrypt = require('bcrypt')
 
 usersRouter.get('/', async (request, response) => {
   const users = await User.find({})
   response.json(users)
 })
 
-usersRouter.post('/', async (request, response, next) => {
+usersRouter.post('/', async (request, response) => {
   const { username, name, password } = request.body
+
+  if (!password) response.status(400).json({ error: 'Password is required' })
+
+  if (password.length < 3)
+    response.status(400).json({ error: 'Password must at least 3 characters' })
 
   const saltRounds = 10
   const passwordHash = await bcrypt.hash(password, saltRounds)
