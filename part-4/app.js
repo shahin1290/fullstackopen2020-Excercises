4c29cc7668762a1cd908b9411ac1cdcefb1b3ccb
shahin1290 <s.patowary@yahoo.com>
2020-11-19 10:07:52 +0100

    finishes 4.18: bloglist expansion, step6

--- part-4/app.js
+++ part-4/app.js
@@ -7,6 +7,7 @@ const cors = require('cors')
 const mongoose = require('mongoose')
 const blogsRouter = require('./controllers/blogs')
 const usersRouter = require('./controllers/users')
+const loginRouter = require('./controllers/login')
 const middleware = require('./utils/middleware')
 
 logger.info('connecting to', config.MONGODB_URI)
@@ -30,6 +31,7 @@ app.use(express.json())
 
 app.use('/api/blogs', blogsRouter)
 app.use('/api/users', usersRouter)
+app.use('/api/login', loginRouter)
 
 app.use(middleware.unknownEndpoint)
 app.use(middleware.errorHandler)
