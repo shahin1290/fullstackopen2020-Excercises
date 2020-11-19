5b83aaa61c8c1f394b94a38ac0bc2df5f5a6e70d
shahin1290 <s.patowary@yahoo.com>
2020-11-19 09:20:48 +0100

    finishes 4.17: bloglist expansion, step5

--- part-4/models/user.js
+++ part-4/models/user.js
@@ -5,6 +5,12 @@ const userSchema = new mongoose.Schema({
   username: { type: String, unique: true, minlength: 3, required: true },
   name: String,
   passwordHash: String,
+  blogs: [
+    {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: 'Blog'
+    }
+  ],
 })
 
 userSchema.plugin(uniqueValidator)
