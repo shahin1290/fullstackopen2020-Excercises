d1b24f8b803a98c8a0b8d9ed2ebf1fdee874c129
shahin1290 <s.patowary@yahoo.com>
2020-11-19 12:21:02 +0100

    finishes 4.20: bloglist expansion, step8

--- part-4/controllers/blogs.js
+++ part-4/controllers/blogs.js
@@ -3,23 +3,16 @@ const Blog = require('../models/blog')
 const User = require('../models/user')
 const jwt = require('jsonwebtoken')
 
-const getTokenFrom = (request) => {
-  const authorization = request.get('authorization')
-  if (authorization && authorization.toLowerCase().startsWith('bearer ')) {
-    return authorization.substring(7)
-  }
-  return null
-}
-
 blogsRouter.get('/', async (request, response) => {
   const blogs = await Blog.find({}).populate('user', { username: 1, name: 1 })
 
   response.json(blogs)
 })
 
-blogsRouter.post('/', async (request, response) => {
+blogsRouter.post('/', async (request, response, next) => {
   const { title, author, url, likes } = request.body
-  const token = getTokenFrom(request)
+  const token = request.token
+
   const decodedToken = jwt.verify(token, process.env.SECRET)
 
   if (!token || !decodedToken.id) {
@@ -27,7 +20,7 @@ blogsRouter.post('/', async (request, response) => {
   }
 
   const user = await User.findById(decodedToken.id)
-  
+
   const blog = new Blog({ title, author, url, likes, user: user._id })
   const savedBlog = await blog.save()
 
