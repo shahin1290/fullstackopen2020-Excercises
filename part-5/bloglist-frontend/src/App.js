9f61c0b6b011cad65bb38aa6f2a7a9e014c56a56
shahin1290 <s.patowary@yahoo.com>
2020-11-20 14:15:32 +0100

    finishes 5.1: bloglist frontend, step1

---
+++ part-5/bloglist-frontend/src/App.js
@@ -0,0 +1,42 @@
+import React, { useState, useEffect } from 'react'
+import Blog from './components/Blog'
+import blogService from './services/blogs'
+import Login from './components/Login'
+
+const App = () => {
+  const [blogs, setBlogs] = useState([])
+  const [username, setUsername] = useState('')
+  const [password, setPassword] = useState('')
+  const [user, setUser] = useState(null)
+
+  useEffect(() => {
+    blogService.getAll().then((blogs) => setBlogs(blogs))
+  }, [])
+
+  if (user === null) {
+    return (
+      <div>
+        <h2>Log in to application</h2>
+        <Login
+          username={username}
+          password={password}
+          setUsername={setUsername}
+          setPassword={setPassword}
+          setUser={setUser}
+        />
+      </div>
+    )
+  }
+
+  return (
+    <div>
+      <h2>blogs</h2>
+      <p>{user.name} logged in</p>
+      {blogs.map((blog) => (
+        <Blog key={blog.id} blog={blog} />
+      ))}
+    </div>
+  )
+}
+
+export default App
---
+++ part-5/bloglist-frontend/src/components/Login.js
@@ -0,0 +1,34 @@
+import React from 'react'
+import loginService from '../services/login'
+
+const Login = ({ setUsername, setPassword, username, password, setUser }) => {
+  const handleNameChange = (e) => {
+    setUsername(e.target.value)
+  }
+
+  const handleNumberChange = (e) => {
+    setPassword(e.target.value)
+  }
+
+  const handleFormSubmit = async (e) => {
+    e.preventDefault()
+    const user = await loginService.login({ username, password })
+    setUser(user)
+  }
+
+  return (
+    <form onSubmit={handleFormSubmit}>
+      <div>
+        username <input onChange={handleNameChange} />
+      </div>
+      <div>
+        password <input onChange={handleNumberChange} />
+      </div>
+      <div>
+        <button type='submit'>login</button>
+      </div>
+    </form>
+  )
+}
+
+export default Login
