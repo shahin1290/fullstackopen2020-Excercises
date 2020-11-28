88ecbb326cd6c2335ffd64f988c2169800979558
shahin1290 <s.patowary@yahoo.com>
2020-11-28 22:23:12 +0100

    finishes 6.15 Anecdotes and the backend, step3

--- part-6/redux-anecdotes/src/reducers/anecdoteReducer.js
+++ part-6/redux-anecdotes/src/reducers/anecdoteReducer.js
@@ -1,3 +1,5 @@
+import anecdoteService from '../services/anecdotes'
+
 const anecdoteReducer = (state = [], action) => {
   switch (action.type) {
     case 'INIT_ANECDOTES':
@@ -21,8 +23,11 @@ const anecdoteReducer = (state = [], action) => {
   }
 }
 
-export const initializeAnecdotes = (anecdotes) => {
-  return { type: 'INIT_ANECDOTES', data: anecdotes }
+export const initializeAnecdotes = () => {
+  return async dispatch => {
+    const anecdotes = await anecdoteService.getAll()
+    dispatch({ type: 'INIT_ANECDOTES', data: anecdotes })
+  }
 }
 
 export const addVote = (id) => {
