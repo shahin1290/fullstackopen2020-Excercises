56d275fde6dbe9bef7d86f063377bf8901afbac6
shahin1290 <s.patowary@yahoo.com>
2020-11-28 20:27:42 +0100

    finishes 6.13 Anecdotes and the backend, step1

--- part-6/redux-anecdotes/src/reducers/anecdoteReducer.js
+++ part-6/redux-anecdotes/src/reducers/anecdoteReducer.js
@@ -1,26 +1,7 @@
-const anecdotesAtStart = [
-  'If it hurts, do it more often',
-  'Adding manpower to a late software project makes it later!',
-  'The first 90 percent of the code accounts for the first 90 percent of the development time...The remaining 10 percent of the code accounts for the other 90 percent of the development time.',
-  'Any fool can write code that a computer can understand. Good programmers write code that humans can understand.',
-  'Premature optimization is the root of all evil.',
-  'Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.',
-]
-
-const getId = () => (100000 * Math.random()).toFixed(0)
-
-const asObject = (anecdote) => {
-  return {
-    content: anecdote,
-    id: getId(),
-    votes: 0,
-  }
-}
-
-const initialState = anecdotesAtStart.map(asObject)
-
-const anecdoteReducer = (state = initialState, action) => {
+const anecdoteReducer = (state = [], action) => {
   switch (action.type) {
+    case 'INIT_ANECDOTES':
+      return action.data
     case 'ADD_VOTE': {
       const { id } = action.data
       const anecdoteToChange = state.find((n) => n.id === id)
@@ -40,11 +21,16 @@ const anecdoteReducer = (state = initialState, action) => {
   }
 }
 
+export const initializeAnecdotes = (anecdotes) => {
+  return { type: 'INIT_ANECDOTES', data: anecdotes }
+}
+
 export const addVote = (id) => {
   return { type: 'ADD_VOTE', data: { id } }
 }
 
 export const createAnecdote = (content) => {
+  const getId = () => (100000 * Math.random()).toFixed(0)
   return { type: 'NEW_ANECDOTE', data: { content, id: getId(), votes: 0 } }
 }
 
--- part-6/redux-anecdotes/src/store.js
+++ part-6/redux-anecdotes/src/store.js
@@ -3,13 +3,19 @@ import anecdoteReducer from './reducers/anecdoteReducer'
 import notificationReducer from './reducers/notificationReducer'
 import filterReducer from './reducers/filterReducer'
 import { composeWithDevTools } from 'redux-devtools-extension'
+import anecdoteService from './services/anecdotes'
+import { initializeAnecdotes } from './reducers/anecdoteReducer'
 
 const reducer = combineReducers({
   anecdotes: anecdoteReducer,
   notification: notificationReducer,
-  filter: filterReducer
+  filter: filterReducer,
 })
 
 const store = createStore(reducer, composeWithDevTools())
+
+anecdoteService
+  .getAll()
+  .then((anecdotes) => store.dispatch(initializeAnecdotes(anecdotes)))
 
 export default store
