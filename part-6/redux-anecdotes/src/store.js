5dc0d1643d9549c2f19c251e4d38944004e59468
shahin1290 <s.patowary@yahoo.com>
2020-11-28 20:42:21 +0100

    moves initialize anecdotes from store to app.js

--- part-6/redux-anecdotes/src/store.js
+++ part-6/redux-anecdotes/src/store.js
@@ -3,8 +3,6 @@ import anecdoteReducer from './reducers/anecdoteReducer'
 import notificationReducer from './reducers/notificationReducer'
 import filterReducer from './reducers/filterReducer'
 import { composeWithDevTools } from 'redux-devtools-extension'
-import anecdoteService from './services/anecdotes'
-import { initializeAnecdotes } from './reducers/anecdoteReducer'
 
 const reducer = combineReducers({
   anecdotes: anecdoteReducer,
@@ -13,9 +11,5 @@ const reducer = combineReducers({
 })
 
 const store = createStore(reducer, composeWithDevTools())
-
-anecdoteService
-  .getAll()
-  .then((anecdotes) => store.dispatch(initializeAnecdotes(anecdotes)))
 
 export default store
