cfffdb8cbfb543dc3feb3fb4e6b8a9e1ef1ace9f
shahin1290 <s.patowary@yahoo.com>
2020-11-30 14:34:55 +0100

    7.2: routed anecdotes, step2

--- part-7/routed-anecdotes/src/App.js
+++ part-7/routed-anecdotes/src/App.js
@@ -1,5 +1,11 @@
 import React, { useState } from 'react'
-import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom'
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link,
+  useParams,
+} from 'react-router-dom'
 
 const Menu = () => {
   const padding = {
@@ -25,11 +31,25 @@ const AnecdoteList = ({ anecdotes }) => (
     <h2>Anecdotes</h2>
     <ul>
       {anecdotes.map((anecdote) => (
-        <li key={anecdote.id}>{anecdote.content}</li>
+        <li key={anecdote.id}>
+          <Link to={`/anecdotes/${anecdote.id}`}>{anecdote.content}</Link>
+        </li>
       ))}
     </ul>
   </div>
 )
+
+const Anecdote = ({ anecdotes }) => {
+  const id = useParams().id
+
+  const anecdote = anecdotes.find((n) => n.id === id)
+  return (
+    <div>
+      <h2>{anecdote.content}</h2>
+      <div>has {anecdote.votes} votes</div>
+    </div>
+  )
+}
 
 const About = () => (
   <div>
@@ -161,6 +181,9 @@ const App = () => {
       <Switch>
         <Route exact path='/'>
           <AnecdoteList anecdotes={anecdotes} />
+        </Route>
+        <Route exact path='/anecdotes/:id'>
+          <Anecdote anecdotes={anecdotes} />
         </Route>
         <Route exact path='/about'>
           <About />
