31e415e5d14169b5ea4afb33124f5ee4eb5fe526
shahin1290 <s.patowary@yahoo.com>
2020-12-01 17:34:48 +0100

    finishes 7.4: anecdotes and hooks step1

--- part-7/routed-anecdotes/src/App.js
+++ part-7/routed-anecdotes/src/App.js
@@ -1,4 +1,5 @@
 import React, { useState } from 'react'
+import { useField } from './hooks'
 import {
   Switch,
   Route,
@@ -85,19 +86,20 @@ const Footer = () => (
 )
 
 const CreateNew = (props) => {
-  const [content, setContent] = useState('')
-  const [author, setAuthor] = useState('')
-  const [info, setInfo] = useState('')
+  const content = useField('text')
+  const author = useField('text')
+  const info = useField('text')
+
   const history = useHistory()
 
   const handleSubmit = (e) => {
     e.preventDefault()
-    props.addNew({
-      content,
-      author,
-      info,
+     props.addNew({
+      content:content.value,
+      author: author.value,
+      info: info.value,
       votes: 0,
-    })
+    }) 
     history.push('/')
   }
 
@@ -107,27 +109,15 @@ const CreateNew = (props) => {
       <form onSubmit={handleSubmit}>
         <div>
           content
-          <input
-            name='content'
-            value={content}
-            onChange={(e) => setContent(e.target.value)}
-          />
+          <input name='content' {...content} />
         </div>
         <div>
           author
-          <input
-            name='author'
-            value={author}
-            onChange={(e) => setAuthor(e.target.value)}
-          />
+          <input name='author' {...author} />
         </div>
         <div>
           url for more info
-          <input
-            name='info'
-            value={info}
-            onChange={(e) => setInfo(e.target.value)}
-          />
+          <input name='info' {...info} />
         </div>
         <button>create</button>
       </form>
