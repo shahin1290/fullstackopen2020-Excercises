39fc894aa065c6a498909857400fa9794a0b38ad
shahin1290 <s.patowary@yahoo.com>
2021-01-17 23:00:12 +0100

    finishes 9.18: patientor, step3

--- part-9/patientor-client/src/state/reducer.ts
+++ part-9/patientor-client/src/state/reducer.ts
@@ -1,19 +1,29 @@
-import { State } from "./state";
-import { Patient } from "../types";
+import { State } from './state';
+import { Patient } from '../types';
 
 export type Action =
   | {
-      type: "SET_PATIENT_LIST";
+      type: 'SET_PATIENT_LIST';
       payload: Patient[];
     }
   | {
-      type: "ADD_PATIENT";
+      type: 'ADD_PATIENT';
       payload: Patient;
     };
 
+export const setPatientList = (patientList: Patient[]): Action => ({
+  type: 'SET_PATIENT_LIST',
+  payload: patientList,
+});
+
+export const addPatient = (patient: Patient): Action => ({
+  type: 'ADD_PATIENT',
+  payload: patient,
+});
+
 export const reducer = (state: State, action: Action): State => {
   switch (action.type) {
-    case "SET_PATIENT_LIST":
+    case 'SET_PATIENT_LIST':
       return {
         ...state,
         patients: {
@@ -21,16 +31,16 @@ export const reducer = (state: State, action: Action): State => {
             (memo, patient) => ({ ...memo, [patient.id]: patient }),
             {}
           ),
-          ...state.patients
-        }
+          ...state.patients,
+        },
       };
-    case "ADD_PATIENT":
+    case 'ADD_PATIENT':
       return {
         ...state,
         patients: {
           ...state.patients,
-          [action.payload.id]: action.payload
-        }
+          [action.payload.id]: action.payload,
+        },
       };
     default:
       return state;
